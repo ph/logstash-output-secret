c4f4883b8acbe070622c8f5b94247ef680910023
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-11-14 16:40:37 -0500

    Similar a uber zip for xpack that will contains the plugins for ES/kibana

--- bundle.sh
+++ bundle.sh
@@ -1,12 +1,20 @@
+export VERSION=5.0.1
 bundle install --path vendor
 bundle exec rake vendor
 bundle exec rake paquet:vendor
 rm -rf build/
-mkdir -p build/logstash-output-secret
-cp -r dependencies build/logstash-output-secret
+mkdir -p build/x-pack-$VERSION/logstash/logstash-monitoring
+cp -r dependencies build/x-pack*/logstash/logstash-monitoring
 gem build logstash-output-secret.gemspec
-mv logstash-output-secret*.gem build/logstash-output-secret
+mv logstash-output-secret*.gem build/x-pack-*/logstash/logstash-monitoring
+
+# Generate stuff for a uber zip
+mkdir -p build/x-pack-$VERSION/elasticsearch
+touch build/x-pack-$VERSION/elasticsearch/README.md
+
+mkdir -p build/x-pack-$VERSION/kibana
+touch build/x-pack-$VERSION/kibana/README.md
+
 cd build/
-#tar -czvf logstash-output-secret-5.0.0.tar.gz logstash-output-secret
-zip -r logstash-output-secret-5.0.0.zip logstash-output-secret
+zip -r x-pack-$VERSION.zip x-pack-$VERSION
 cd ..
